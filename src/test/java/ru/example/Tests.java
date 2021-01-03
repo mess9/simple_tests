d991e901aa1786b3178c6168dac69a7b925de6b1
mess9 <as0509@ya.ru>
2021-01-03 10:13:11 +0300

    fix

--- src/test/java/ru/example/Tests.java
+++ src/test/java/ru/example/Tests.java
@@ -9,59 +9,77 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class Tests {
 
     @BeforeEach
-    public void helloWorld(){
+    public void helloWorld() {
         System.out.println("-----------------------------");
         System.out.println("------   TEST RUN -----------");
         System.out.println("-----------------------------");
     }
 
     @AfterEach
-    public void godbyWorld(){
+    public void godbyWorld() {
         System.out.println("-----------------------------");
         System.out.println("------   TEST END -----------");
         System.out.println("-----------------------------");
     }
 
+    String two = "2";
+    String one = "1";
+
+    public String listenMavenParam() {
+        String pass = System.getProperty("pass");
+        if (pass.equals("yes")) {
+            return one;
+        }
+        return two;
+    }
+
     @Test
     public void test1() {
-        assertEquals("1","2", "fail test");
+        assertEquals("1", listenMavenParam());
     }
+
     @Test
     public void test2() {
-        assertEquals("1","2", "fail test");
+        assertEquals("1", listenMavenParam());
     }
+
     @Test
     public void test3() {
-        assertEquals("1","2", "fail test");
+        assertEquals("1", listenMavenParam());
     }
+
     @Test
     public void test4() {
-        assertEquals("1","1", "pass test");
+        assertEquals("1", "1", "pass test");
     }
+
     @Test
     public void test5() {
-        assertEquals("1","1", "pass test");
+        assertEquals("1", "1", "pass test");
     }
+
     @Test
     public void test6() {
-        assertEquals("1","1", "pass test");
+        assertEquals("1", "1", "pass test");
     }
+
     @Test
     public void test7() {
-        assertEquals("1","1", "pass test");
+        assertEquals("1", "1", "pass test");
     }
+
     @Test
     public void test8() {
-        assertEquals("1","1", "pass test");
+        assertEquals("1", "1", "pass test");
     }
+
     @Test
     public void test9() {
         System.getenv().forEach((k, v) -> {
             System.out.println(k + ":" + v);
         });
-        assertEquals("1","1", "pass test");
+        assertEquals("1", "1", "pass test");
     }
-
 
 
 }
