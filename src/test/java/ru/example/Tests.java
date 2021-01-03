45fa4197936d0f5028ed505eb0b3a5f3e56be5eb
mess9 <as0509@ya.ru>
2021-01-03 09:55:58 +0300

    fix

--- src/test/java/ru/example/Tests.java
+++ src/test/java/ru/example/Tests.java
@@ -1,10 +1,26 @@
 package ru.example;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Tests {
+
+    @BeforeEach
+    public void helloWorld(){
+        System.out.println("-----------------------------");
+        System.out.println("------   TEST RUN -----------");
+        System.out.println("-----------------------------");
+    }
+
+    @AfterEach
+    public void godbyWorld(){
+        System.out.println("-----------------------------");
+        System.out.println("------   TEST END -----------");
+        System.out.println("-----------------------------");
+    }
 
     @Test
     public void test1() {
